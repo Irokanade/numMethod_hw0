b6fb84c6db7a56cc51958a6374f60021c7576dc7
irokanade <michaeljs.leong@gmail.com>
2022-09-16 16:54:24 +0800

    init commit

--- numMethod_hw0/main.c
+++ numMethod_hw0/main.c
@@ -7,8 +7,37 @@
 
 #include <stdio.h>
 
+
+double hornerMethod(int deg, double x, double coeff[], int index);
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    //input data
+    int deg;
+    double x;
+    
+    printf("enter poly deg\n");
+    scanf("%d", &deg);
+    printf("enter x\n");
+    scanf("%lf", &x);
+    
+    double coeff[deg+1];
+    
+    //input coeff arr
+    for(int i = 0; i < deg+1; i++) {
+        printf("enter coeff for x deg %d\n", i);
+        scanf("%lf", &coeff[i]);
+    }
+    
+    printf("%lf\n", hornerMethod(deg, x, coeff, 0));
+    
     return 0;
+}
+
+double hornerMethod(int deg, double x, double coeff[], int index) {
+    
+    if(deg > 0) {
+        return (hornerMethod(deg-1, x, coeff, index+1)*x)+coeff[index];
+    }
+    
+    return coeff[index];
 }
